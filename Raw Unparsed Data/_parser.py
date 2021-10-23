297d20e87fd353559d7a080b44b879dcb00b170d
Alex Udodik <alex.udodik@wsu.edu>
2021-10-22 20:37:10 -0700

    Added empty create_tables.sql file

--- Raw Unparsed Data/_parser.py
+++ Raw Unparsed Data/_parser.py
@@ -3,8 +3,8 @@ import csv
 with open('Accessories.csv', mode ='r') as file:
     csvFile = csv.reader(file)
 
-    count = 0
+ 
     for lines in csvFile:
-        count = count + 1
-    print(count)   
+        print(lines)
+        
 
