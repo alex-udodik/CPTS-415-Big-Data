cacc436fc54174dbde2c0aa37d712884adbc1690
Alex Udodik <alex.udodik@wsu.edu>
2021-10-22 20:43:34 -0700

    Added skeleton tables

---
+++ Create Tables Statements/create_tables.sql
@@ -0,0 +1,119 @@
+CREATE TABLE Accessories (
+
+);
+
+CREATE TABLE Achievements (
+
+);
+
+CREATE TABLE Art (
+
+);
+
+CREATE TABLE Bags (
+
+);
+
+CREATE TABLE Bottoms (
+
+);
+
+CREATE TABLE Construction (
+
+);
+
+CREATE TABLE DressUp (
+
+);
+
+CREATE TABLE Fencing (
+
+);
+
+CREATE TABLE Fish (
+
+);
+
+CREATE TABLE Floors (
+
+);
+
+CREATE TABLE Fossils (
+
+);
+
+CREATE TABLE Headwear (
+
+);
+
+CREATE TABLE Housewares (
+
+);
+
+CREATE TABLE Insects (
+
+);
+
+CREATE TABLE Miscellaneous (
+
+);
+
+CREATE TABLE Music (
+
+);
+
+CREATE TABLE Other (
+
+);
+
+CREATE TABLE Photos (
+
+);
+
+CREATE TABLE Posters (
+
+);
+
+CREATE TABLE Reactions (
+
+);
+
+CREATE TABLE Recipes (
+
+);
+
+CREATE TABLE Rugs (
+
+);
+
+CREATE TABLE Shoes (
+
+);
+
+CREATE TABLE Socks (
+
+);
+
+CREATE TABLE Tools (
+
+);
+
+CREATE TABLE Tops (
+
+);
+
+CREATE TABLE Umbreallas (
+
+);
+
+CREATE TABLE Villagers (
+
+);
+
+CREATE TABLE WallMounted (
+
+);
+
+CREATE TABLE Wallpaper (
+
+);
