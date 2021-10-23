a3a4d09e31c20f6032a2de00580bfc176109fd74
Alex Udodik <alex.udodik@wsu.edu>
2021-10-22 22:00:28 -0700

    Implemented floors table

--- Create Tables Statements/create_tables.sql
+++ Create Tables Statements/create_tables.sql
@@ -219,7 +219,25 @@ CREATE TABLE Fish (
 );
 
 CREATE TABLE Floors (
-
+    Name VARCHAR(255),
+    VFX BOOLEAN,
+    DIY BOOLEAN,
+    Buy VARCHAR(255), /* Might need to be changed */
+    Sell INTEGER,
+    Color1 VARCHAR(255),
+    Color2 VARCHAR(255),
+    MilesPrice VARCHAR(255), /* Might need to be changed */
+    Source VARCHAR(255),
+    SourceNotes VARCHAR(255),
+    Version VARCHAR(255),
+    HHAConcept1 VARCHAR(255),
+    HHAConcept2 VARCHAR(255),
+    HHASeries VARCHAR(255),
+    Tag VARCHAR(255),
+    Catalog VARCHAR(255),
+    Filename VARCHAR(255),
+    InternalID INTEGER,
+    UniqueEntryID VARCHAR(255) UNIQUE
 );
 
 CREATE TABLE Fossils (
