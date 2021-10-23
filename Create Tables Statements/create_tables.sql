399731b1fd07b84fc64687087dec4d1ed5c568aa
Alex Udodik <alex.udodik@wsu.edu>
2021-10-22 22:30:22 -0700

    Implemented photos table

--- Create Tables Statements/create_tables.sql
+++ Create Tables Statements/create_tables.sql
@@ -429,7 +429,27 @@ CREATE TABLE Other (
 );
 
 CREATE TABLE Photos (
-
+    Name VARCHAR(255),
+    Variation VARCHAR(255),
+    BodyTitle VARCHAR(255),
+    Pattern VARCHAR(255),
+    PatternTitle VARCHAR(255),
+    DIY BOOLEAN,
+    Customize BOOLEAN,
+    KitCost INTEGER,
+    Buy VARCHAR(255), /* Might need to be changed */
+    Sell INTEGER,
+    Color1 VARCHAR(255),
+    Color2 VARCHAR(255),
+    Size VARCHAR(255),
+    Source VARCHAR(255),
+    SourceNotes VARCHAR(255),
+    Version VARCHAR(255),
+    Catalog VARCHAR(255),
+    Filename VARCHAR(255),
+    VariantID VARCHAR(255),
+    InternalID INTEGER,
+    UniqueEntryID VARCHAR(255) UNIQUE
 );
 
 CREATE TABLE Posters (
