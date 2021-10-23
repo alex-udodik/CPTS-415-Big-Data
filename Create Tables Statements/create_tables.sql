68a41aeca249c01966a11a2e15b3f4ffe0770c05
Alex Udodik <alex.udodik@wsu.edu>
2021-10-22 22:43:47 -0700

    Implemented shoes table

--- Create Tables Statements/create_tables.sql
+++ Create Tables Statements/create_tables.sql
@@ -527,7 +527,27 @@ CREATE TABLE Rugs (
 );
 
 CREATE TABLE Shoes (
-
+    Name VARCHAR(255),
+    Variation VARCHAR(255),
+    DIY BOOLEAN,
+    Buy INTEGER,
+    Sell INTEGER,
+    Color1 VARCHAR(255),
+    Color2 VARCHAR(255),
+    Size VARCHAR(255), /* Might need to be changed */
+    MilesPrice VARCHAR(255), /* Might need to be changed */
+    Source VARCHAR(255),
+    SourceNotes VARCHAR(255),
+    SeasonalAvailability VARCHAR(255),
+    MannequinPiece BOOLEAN,
+    Version VARCHAR(255),
+    Style VARCHAR(255),
+    LabelThemes VARCHAR(255), /* Might beed to be changed */
+    VillagerEquippable BOOLEAN,
+    Catalog VARCHAR(255),
+    Filename VARCHAR(255),
+    InternalID INTEGER,
+    UniqueEntryID VARCHAR(255) UNIQUE
 );
 
 CREATE TABLE Socks (
