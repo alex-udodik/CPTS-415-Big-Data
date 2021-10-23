6debd3bee839d55cd1596a8a99e3731e85e99915
Alex Udodik <alex.udodik@wsu.edu>
2021-10-22 21:02:36 -0700

    Implemented table for accessories

--- Create Tables Statements/create_tables.sql
+++ Create Tables Statements/create_tables.sql
@@ -1,5 +1,26 @@
 CREATE TABLE Accessories (
-
+    Name VARCHAR(255),
+    Variation VARCHAR(255),
+    DIY BOOLEAN,
+    Buy INTEGER,
+    Sell INTEGER,
+    Color1 VARCHAR(255),
+    Color2 VARCHAR(255),
+    UniqueValue VARCHAR(255), /* Might need to be changed */
+    MilesPrice VARCHAR(255), /* Might need to be changed */
+    Source VARCHAR(255),
+    SourceNotes VARCHAR(255),
+    SeasonalAvailability VARCHAR(255),
+    MannequinPiece BOOLEAN,
+    Version VARCHAR(255),
+    Style VARCHAR(255),
+    LabelThemes VARCHAR(255),
+    Type VARCHAR(255),
+    VillagerEquippable BOOLEAN,
+    Catelog BOOLEAN, /* Might need to be changed */
+    Filename VARCHAR(255),
+    InternalID INTEGER,
+    UniqueEntryID VARCHAR(255) UNIQUE
 );
 
 CREATE TABLE Achievements (
