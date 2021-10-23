5f8c378e7035841384e69656f6b97869327e25ed
Alex Udodik <alex.udodik@wsu.edu>
2021-10-22 21:24:09 -0700

    Implemented Bags table

--- Create Tables Statements/create_tables.sql
+++ Create Tables Statements/create_tables.sql
@@ -77,7 +77,26 @@ CREATE TABLE Art (
 );
 
 CREATE TABLE Bags (
-
+    Name VARCHAR(255),
+    Variation VARCHAR(255),
+    DIY BOOLEAN,
+    Buy VARCHAR,
+    Sell INTEGER,
+    Color1 VARCHAR(255),
+    Color2 VARCHAR(255),
+    Size VARCHAR(255), /* Might need to be changed */
+    MilesPrice VARCHAR(255),
+    Source VARCHAR(255),
+    SourceNotes VARCHAR(255),
+    SeasonalAvailability VARCHAR(255),
+    Version VARCHAR(255),
+    Style VARCHAR(255),
+    LabelThemes VARCHAR(255),
+    VillagerEquippable BOOLEAN,
+    Catalog VARCHAR(255),
+    Filename VARCHAR(255),
+    InternalID INTEGER,
+    UniqueEntryID VARCHAR(255)
 );
 
 CREATE TABLE Bottoms (
