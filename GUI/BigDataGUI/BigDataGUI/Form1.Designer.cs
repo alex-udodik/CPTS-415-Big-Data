41186ab20cdc48f40d6d6c3a82d8550d3bac1f6c
alex-udodik <alex.udodik@wsu.edu>
2021-12-10 18:47:14 -0800

    Deleted extra UI stuff

--- GUI/BigDataGUI/BigDataGUI/Form1.Designer.cs
+++ GUI/BigDataGUI/BigDataGUI/Form1.Designer.cs
@@ -35,11 +35,6 @@ namespace BigDataGUI
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.checkBox5 = new System.Windows.Forms.CheckBox();
-            this.label5 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.label6 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.label8 = new System.Windows.Forms.Label();
             this.comboBoxSearch = new System.Windows.Forms.ComboBox();
@@ -60,21 +55,19 @@ namespace BigDataGUI
             // dataGridViewSearchResultsLeft
             // 
             this.dataGridViewSearchResultsLeft.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridViewSearchResultsLeft.Location = new System.Drawing.Point(8, 50);
-            this.dataGridViewSearchResultsLeft.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataGridViewSearchResultsLeft.Location = new System.Drawing.Point(6, 41);
             this.dataGridViewSearchResultsLeft.Name = "dataGridViewSearchResultsLeft";
             this.dataGridViewSearchResultsLeft.RowHeadersWidth = 51;
-            this.dataGridViewSearchResultsLeft.Size = new System.Drawing.Size(680, 816);
+            this.dataGridViewSearchResultsLeft.Size = new System.Drawing.Size(510, 663);
             this.dataGridViewSearchResultsLeft.TabIndex = 1;
             this.dataGridViewSearchResultsLeft.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewSearchResultsLeft_CellClick);
             // 
             // searchTextBox
             // 
             this.searchTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.searchTextBox.Location = new System.Drawing.Point(519, 26);
-            this.searchTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.searchTextBox.Location = new System.Drawing.Point(389, 21);
             this.searchTextBox.Name = "searchTextBox";
-            this.searchTextBox.Size = new System.Drawing.Size(575, 36);
+            this.searchTextBox.Size = new System.Drawing.Size(432, 30);
             this.searchTextBox.TabIndex = 2;
             this.searchTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
             // 
@@ -82,10 +75,9 @@ namespace BigDataGUI
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(411, 32);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(308, 26);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(93, 29);
+            this.label1.Size = new System.Drawing.Size(75, 25);
             this.label1.TabIndex = 3;
             this.label1.Text = "Search";
             // 
@@ -93,10 +85,9 @@ namespace BigDataGUI
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(8, 20);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(6, 16);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(135, 29);
+            this.label2.Size = new System.Drawing.Size(107, 25);
             this.label2.TabIndex = 4;
             this.label2.Text = "Categories";
             // 
@@ -104,10 +95,9 @@ namespace BigDataGUI
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(8, 20);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(6, 16);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(160, 26);
+            this.label3.Size = new System.Drawing.Size(132, 22);
             this.label3.TabIndex = 5;
             this.label3.Text = "Search Results";
             // 
@@ -115,64 +105,11 @@ namespace BigDataGUI
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(697, 20);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Location = new System.Drawing.Point(523, 16);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(128, 26);
+            this.label4.Size = new System.Drawing.Size(103, 22);
             this.label4.TabIndex = 6;
             this.label4.Text = "Item Details";
-            // 
-            // checkBox5
-            // 
-            this.checkBox5.AutoSize = true;
-            this.checkBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.checkBox5.Location = new System.Drawing.Point(8, 229);
-            this.checkBox5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.checkBox5.Name = "checkBox5";
-            this.checkBox5.Size = new System.Drawing.Size(66, 29);
-            this.checkBox5.TabIndex = 17;
-            this.checkBox5.Text = "DIY";
-            this.checkBox5.UseVisualStyleBackColor = true;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(223, 270);
-            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(118, 25);
-            this.label5.TabIndex = 18;
-            this.label5.Text = "Price Range";
-            // 
-            // textBox2
-            // 
-            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(8, 266);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(67, 30);
-            this.textBox2.TabIndex = 19;
-            // 
-            // textBox3
-            // 
-            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox3.Location = new System.Drawing.Point(131, 266);
-            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(83, 30);
-            this.textBox3.TabIndex = 20;
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(87, 270);
-            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(36, 25);
-            this.label6.TabIndex = 21;
-            this.label6.Text = "To";
             // 
             // groupBox1
             // 
@@ -181,17 +118,10 @@ namespace BigDataGUI
             this.groupBox1.Controls.Add(this.button3);
             this.groupBox1.Controls.Add(this.comboBox1);
             this.groupBox1.Controls.Add(this.label7);
-            this.groupBox1.Controls.Add(this.label6);
-            this.groupBox1.Controls.Add(this.textBox3);
             this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.textBox2);
-            this.groupBox1.Controls.Add(this.label5);
-            this.groupBox1.Controls.Add(this.checkBox5);
-            this.groupBox1.Location = new System.Drawing.Point(16, 15);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.groupBox1.Size = new System.Drawing.Size(384, 955);
+            this.groupBox1.Size = new System.Drawing.Size(288, 776);
             this.groupBox1.TabIndex = 22;
             this.groupBox1.TabStop = false;
             // 
@@ -199,29 +129,27 @@ namespace BigDataGUI
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(184, 55);
-            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Location = new System.Drawing.Point(138, 45);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(92, 25);
+            this.label8.Size = new System.Drawing.Size(73, 20);
             this.label8.TabIndex = 24;
             this.label8.Text = "Category";
             // 
             // comboBoxSearch
             // 
             this.comboBoxSearch.FormattingEnabled = true;
-            this.comboBoxSearch.Location = new System.Drawing.Point(15, 55);
-            this.comboBoxSearch.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.comboBoxSearch.Location = new System.Drawing.Point(11, 45);
             this.comboBoxSearch.Name = "comboBoxSearch";
-            this.comboBoxSearch.Size = new System.Drawing.Size(160, 24);
+            this.comboBoxSearch.Size = new System.Drawing.Size(121, 21);
             this.comboBoxSearch.TabIndex = 23;
             this.comboBoxSearch.SelectedIndexChanged += new System.EventHandler(this.comboBoxSearch_SelectedIndexChanged);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(92, 812);
-            this.button3.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.button3.Location = new System.Drawing.Point(69, 660);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(176, 65);
+            this.button3.Size = new System.Drawing.Size(132, 53);
             this.button3.TabIndex = 11;
             this.button3.Text = "Get Statistics";
             this.button3.UseVisualStyleBackColor = true;
@@ -231,10 +159,9 @@ namespace BigDataGUI
             // 
             this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(15, 89);
-            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.comboBox1.Location = new System.Drawing.Point(11, 72);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(160, 24);
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
             this.comboBox1.TabIndex = 22;
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
@@ -242,10 +169,9 @@ namespace BigDataGUI
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(184, 86);
-            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Location = new System.Drawing.Point(138, 70);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(59, 25);
+            this.label7.Size = new System.Drawing.Size(46, 20);
             this.label7.TabIndex = 22;
             this.label7.Text = "Color";
             // 
@@ -258,20 +184,17 @@ namespace BigDataGUI
             this.groupBox2.Controls.Add(this.dataGridViewSearchResultsLeft);
             this.groupBox2.Controls.Add(this.label3);
             this.groupBox2.Controls.Add(this.label4);
-            this.groupBox2.Location = new System.Drawing.Point(417, 96);
-            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox2.Location = new System.Drawing.Point(313, 78);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.groupBox2.Size = new System.Drawing.Size(1391, 874);
+            this.groupBox2.Size = new System.Drawing.Size(1043, 710);
             this.groupBox2.TabIndex = 23;
             this.groupBox2.TabStop = false;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(1052, 758);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Location = new System.Drawing.Point(789, 616);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(132, 53);
+            this.button2.Size = new System.Drawing.Size(99, 43);
             this.button2.TabIndex = 10;
             this.button2.Text = "Remove From Owned Content";
             this.button2.UseVisualStyleBackColor = true;
@@ -279,10 +202,9 @@ namespace BigDataGUI
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(912, 758);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Location = new System.Drawing.Point(684, 616);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(132, 53);
+            this.button1.Size = new System.Drawing.Size(99, 43);
             this.button1.TabIndex = 9;
             this.button1.Text = "Add to Owned Content";
             this.button1.UseVisualStyleBackColor = true;
@@ -291,33 +213,30 @@ namespace BigDataGUI
             // linkLabelWiki
             // 
             this.linkLabelWiki.AutoSize = true;
-            this.linkLabelWiki.Location = new System.Drawing.Point(843, 26);
-            this.linkLabelWiki.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.linkLabelWiki.Location = new System.Drawing.Point(632, 21);
             this.linkLabelWiki.Name = "linkLabelWiki";
-            this.linkLabelWiki.Size = new System.Drawing.Size(0, 17);
+            this.linkLabelWiki.Size = new System.Drawing.Size(0, 13);
             this.linkLabelWiki.TabIndex = 8;
             this.linkLabelWiki.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabelWiki_LinkClicked);
             // 
             // dataGridViewItemDetails
             // 
             this.dataGridViewItemDetails.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridViewItemDetails.Location = new System.Drawing.Point(703, 50);
-            this.dataGridViewItemDetails.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataGridViewItemDetails.Location = new System.Drawing.Point(527, 41);
             this.dataGridViewItemDetails.Name = "dataGridViewItemDetails";
             this.dataGridViewItemDetails.RowHeadersWidth = 51;
-            this.dataGridViewItemDetails.Size = new System.Drawing.Size(680, 673);
+            this.dataGridViewItemDetails.Size = new System.Drawing.Size(510, 547);
             this.dataGridViewItemDetails.TabIndex = 7;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1824, 985);
+            this.ClientSize = new System.Drawing.Size(1368, 800);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.searchTextBox);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewSearchResultsLeft)).EndInit();
@@ -338,11 +257,6 @@ namespace BigDataGUI
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.CheckBox checkBox5;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.Label label6;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.DataGridView dataGridViewItemDetails;
